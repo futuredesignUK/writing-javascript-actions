bc3c23c2a9439df2b5ff7821476f6008b5786e1a
sam.hope-evans <sam.hope-evans@futuredesign.co.uk>
2021-11-19 12:29:10 +0000

    jokeIssue

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,7 +9,7 @@ async function run() {
   
       const octokit = new github.getOctokit(token);
   
-      const newIssue = await octokit.issues.create({
+      const jokeIssue = await octokit.issues.create({
         repo: github.context.repo.repo,
         owner: github.context.repo.owner,
         title: issueTitle,
